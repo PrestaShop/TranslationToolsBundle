f308263233b88222f120e2c98e326f0df4cd66b2
Mickaël Andrieu <andrieu.travail@gmail.com>
2016-08-26 17:42:29 +0200

    Introduce new Flattenizer

---
+++ Tests/Translation/Extractor/Util/FlattenizerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace PrestaShop\TranslationToolsBundle\Tests\Translation\Extractor;
+
+class FlattenizerTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+    }
+
+    public function tearDown()
+    {
+    }
+
+    public function testFlatten()
+    {
+        $this->markTestSkipped('To be written soon :)')
+    }
+}
---
+++ Translation/Extractor/Util/Flattenizer.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * 2007-2016 PrestaShop.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2015 PrestaShop SA
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+namespace PrestaShop\TranslationToolsBundle\Translation\Extractor\Util;
+
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+
+/**
+ * This class have only one thing to do, transform:.
+ *
+ * ├── Admin
+ * │   └── Catalog
+ * │        ├── Feature.en-US.xlf
+ * │        ├── Help.en-US.xlf
+ * │        └── Notification.en-US.xlf
+ * └── Shop
+ *      └── PDF.en-US.xlf
+ * 
+ *
+ * Into:
+ *
+ * ├── AdminCatalogFeature.en-US.xlf
+ * ├── AdminCatalogHelp.en-US.xlf
+ * ├── AdminCatalogNotification.en-US.xlf
+ * └── ShopPDF.en-US.xlf
+ */
+class Flattenizer
+{
+    /**
+     * @input string $inputPath Path of directory to flattenize
+     * @input string $outPath Location of flattenized files newly created
+     */
+    public static function flatten($inputPath, $outputPath)
+    {
+        $finder = new Finder();
+        $filesystem = new Filesystem();
+
+        $filesystem->remove($outputPath)->mkdir($outputPath);
+
+        foreach ($finder->in($inputPath) as $file) {
+            $flatName = preg_replace('/\//', '', $file->getRelativePath()).$file->getFilename();
+            $filesystem->copy($file->getRealpath(), $outputPath.'/'.$flatName);
+        }
+    }
+}
