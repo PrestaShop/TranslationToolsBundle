ffca245b9f92ef01b6aab7dae889346a243127c4
Mickaël Andrieu <mickael.andrieu@solvolabs.com>
2021-08-05 13:28:09 +0200

    Fixed the deprecation

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -37,8 +37,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('translation_tools');
+        $treeBuilder = new TreeBuilder('translation_tools');
 
         return $treeBuilder;
     }
