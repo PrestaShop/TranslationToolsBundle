3504d90d9b7986b499eb96ae85a8800db971d0cb
Thomas BACCELLI <thomas.baccelli@prestashop.com>
2022-03-14 18:12:34 +0100

    Add Configuration UnitTest

---
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is authored by PrestaShop SA and Contributors <contact@prestashop.com>
+ *
+ * It is distributed under MIT license.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PrestaShop\TranslationToolsBundle\Tests\DependencyInjection;
+
+use PrestaShop\TranslationToolsBundle\DependencyInjection\Configuration;
+use PrestaShop\TranslationToolsBundle\Tests\PhpUnit\TestCase;
+
+class ConfigurationTest extends TestCase
+{
+    public function testNewConfiguration()
+    {
+        $configuration = new Configuration();
+        $this->assertSame('translation_tools', $configuration->getConfigTreeBuilder()->buildTree()->getName());
+    }
+}
