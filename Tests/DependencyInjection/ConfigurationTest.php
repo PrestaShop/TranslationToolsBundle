5578bf62ce243cd625ab4ce5bdb9939538206693
atomiix <tbaccelli@gmail.com>
2022-03-15 10:06:00 +0100

    Apply suggestions from code review
    Co-authored-by: Progi1984 <progi1984@gmail.com>

--- Tests/DependencyInjection/ConfigurationTest.php
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -8,6 +8,8 @@
  * file that was distributed with this source code.
  */
 
+declare(strict_types=1);
+
 namespace PrestaShop\TranslationToolsBundle\Tests\DependencyInjection;
 
 use PrestaShop\TranslationToolsBundle\DependencyInjection\Configuration;
@@ -15,7 +17,7 @@ use PrestaShop\TranslationToolsBundle\Tests\PhpUnit\TestCase;
 
 class ConfigurationTest extends TestCase
 {
-    public function testNewConfiguration()
+    public function testNewConfiguration(): void
     {
         $configuration = new Configuration();
         $this->assertSame('translation_tools', $configuration->getConfigTreeBuilder()->buildTree()->getName());
